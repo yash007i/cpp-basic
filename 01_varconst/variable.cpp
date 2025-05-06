de2a4d3a5a346b6db1ba794f85029bbd19878f6d
yash007i <pyash2268@gmail.com>
2025-05-06 22:05:12 +0530

    Learn about variable and nameing rules

---
+++ 01_varconst/variable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int score; // variable declaration
+    score = 100;
+
+    int chaiPrice = 50;
+    chaiPrice = 100; 
+
+    // Read variable nameing rules
+
+    const int id = 12423859347; // Not changeable throught out the programm
+
+    cout << "Score is : " << score ;
+    return 0;
+}
