ab19fcca0a9ab73a4123f6602130538619d91e6d
yash007i <pyash2268@gmail.com>
2025-05-06 21:51:44 +0530

    C++ basic with help of hello world programm

---
+++ 00_basics_of_cpp/basic.cpp
@@ -0,0 +1,18 @@
+#include <iostream> // pre processor derectives
+
+using namespace std;
+// using std::cout;
+// using std::endl;
+
+// Also make your own namespace 
+// Some namespaces like qt-framework, Eigen
+// namespace mySpace {
+//     void printName () {}
+// }
+
+int main () {
+    cout << "Hello from C++" << endl ;
+    // std::cout << "Hello from C++" << std::endl ;
+    // mySpace::printName();
+    return 0;
+}
